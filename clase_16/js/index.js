42fbcf94169598be2ccd9691b70f84ee690c43c7
Mateo J. Sosa <mateososa134@hotmail.com>
2024-05-06 16:01:33 -0300

    Objetos con JSON
    Primera CLASE con JSON

--- clase_16/js/index.js
+++ clase_16/js/index.js
@@ -34,3 +34,76 @@ console.log(arrayConcat);
 
 /* .sort(func) : Se Ordena segun el criterio de una funcion creada */
 
+
+
+
+
+//-------------------  STORAGE  ----------------------------
+// Inspeccionar - Apliccation - Storage
+
+// Se guardaran ambas por dominio
+
+// LOCAL STORAGE - INDEFINIDA Solo soporta Strings. Guarda entre 5 a 10mb. 
+
+// SESSION STORAGE - DEFINIDA / DURACION DE LA PESTAÑA DEL NAVEGADOR= Guarda entre 5 a 10mb, se guardara en el cliente, minimiza el trafico en la red. Si se desconecta la red.
+
+// Vemos si el web API storga es compatible con el NAVEGADOR
+if (typeof (Storage) != 'undefined') {
+    // let cursos = ["HTML", "CSS", "JS", "VS Code"];
+    // localStorage.setItem("cursosComprados", cursos);
+    // localStorage.clear();
+    // localStorage.removeItem("cursosComprados");
+
+
+    // console.log(localStorage.getItem("cursosComprados"));
+    // let cursosRecuperados = localStorage.getItem("cursosComprados");
+    // console.log(typeof (localStorage.getItem("cursosComprados")));
+
+    /* NO ME DEJARA AGREGAR UN NUEVO ELEMENTO AL ARRAY PORQUE ES UN STRING,
+    EL STORAGE SOLO RECIBIRA STRINGS. Cuando lo suba lo hara en string, pero cuando lo baje para usarlo deberia pasarlo a Array.
+    cursosRecuperados.push("Recuparados");
+
+    SOLUCION: Convertilo en un archivo del tipo JSON
+    JAVASCRIPT OBJECT NOTATION: Es una LENGUAJE INTERMEDIO. Se utiliza para comunicar API's entre si, o el Front con el BackEnd.
+    Formato exclusivo de codigo para guardar datos.
+    */
+
+
+
+    // SOLO DURA DURANTE LA SESION DE LA PAGINA
+    /*
+    let preferenciaColor = ["Negro"];
+    sessionStorage.setItem("preferenciaColor", preferenciaColor);
+    */
+
+} else {
+    alert("STORAGE No es compatible con este navegador");
+}
+
+
+
+// --------------------------  JSON  -------------------------------
+
+if (typeof (Storage) != 'undefined') {
+    let cursos = ["HTML", "CSS", "JS", "VS Code"];
+    // JSON.stringify(): SUBIDO CONVERTIDO A JSON al ARRAY
+    localStorage.setItem("cursosComprados", JSON.stringify(cursos));
+
+
+    // JSON.parse(): BAJADO CONVERTIDO A JSON al String
+    let cursosRecuperados = JSON.parse(localStorage.getItem("cursosComprados"));
+
+    console.log(typeof (cursosRecuperados));
+
+    cursosRecuperados.push("HOLA");
+
+    cursosRecuperados.forEach(curso => {
+        console.log(curso);
+    });
+
+} else {
+    alert("STORAGE No es compatible con este navegador");
+}
+
+
+
---
+++ clase_16/js/index2.js
@@ -0,0 +1,20 @@
+let personaJSON = [{
+    "Nombre": "marcos",
+    "Apellido": "sosa",
+    "Edad": 28,
+    "Profesion": "Ninguna",
+    "Estudiante": "Lic. en Informatica",
+    "Peliculas": ["Saw", "El exorcista"],
+    "Altura": 1.84
+},
+
+{
+    "Nombre": "Jose",
+    "Apellido": "Paz",
+    "Edad": 68,
+    "Profesion": "Cerrajero",
+    "Estudiante": "No",
+    "Peliculas": ["Saw", "El exorcista"],
+    "Altura": 1.78
+}
+]
