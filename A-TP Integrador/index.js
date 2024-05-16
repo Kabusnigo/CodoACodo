698980e005b6d1eb0bc244d967b2a3db22820977
Mateo J. Sosa <mateososa134@hotmail.com>
2024-05-16 20:38:19 -0300

    creado mi branch del tp

--- A-TP Integrador/index.js
+++ A-TP Integrador/index.js
@@ -1,4 +1,4 @@
-let sliderInner = document.querySelector(".slider--inner");
+/*let sliderInner = document.querySelector(".slider--inner");
 
 let images = document.querySelectorAll(".imgC");
 
@@ -14,7 +14,54 @@ setInterval(function slider() {
         index = 0;
     }
 
-}, 4500);
+}, 4500);*/
+
+const sliderInner = document.querySelectorAll('.slider--inner');
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+
+function autoslide() {
+    counter += 1;
+    slidefun(counter);
+}
+
+function plusSlides(n) {
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function currentSilde(n) {
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(autoslide, 8000);
+}
+
+function slidefun(n) {
+    for (let i = 0; i < sliderInner.length; i++) {
+        sliderInner[i].style.display = "none";
+    }
+
+    if (n > sliderInner.length) {
+        counter = 1;
+    }
+
+    if (n < 1) {
+        counter = sliderInner.length;
+    }
+
+    sliderInner[counter - 1].style.display = "block";
+}
+
+//          FIN DEL SLIDE
 
 function validateForm(event){
     event.preventDefault();
